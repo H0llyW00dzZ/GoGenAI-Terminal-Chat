f491feda5cf96294b2fd012987fdab11b89b1923
H0llyW00dzZ <priv8@btz.pm>
2024-01-02 23:09:29 +0700

    Feat [Terminal] Constants
    - [+] feat(terminal): add constants for terminal package
    - [+] feat(terminal): add constants for language

---
+++ terminal/constant.go
@@ -0,0 +1,15 @@
+package terminal
+
+// Defined constants for the terminal package
+const (
+	SignalMessage = "\nReceived an interrupt, shutting down gracefully..."
+	StripChars    = "---"
+	NewLineChars  = '\n'
+)
+
+// Defined constants for language
+
+const (
+	YouNerd = "🤓 You: "
+	AiNerd  = "🤖 AI: "
+)
