9a632017abd53f6a9a56af69b9e6bfac2ca64e4b
H0llyW00dzZ <priv8@btz.pm>
2024-01-02 23:14:35 +0700

    Chore [Terminal] [Constants] Update Constants
    - [+] chore(constant.go): add animated chars and model AI constants
    - [+] docs(constant.go): add comments for animated chars and model AI constants

--- terminal/constant.go
+++ terminal/constant.go
@@ -5,6 +5,10 @@ const (
 	SignalMessage = "\nReceived an interrupt, shutting down gracefully..."
 	StripChars    = "---"
 	NewLineChars  = '\n'
+	// this animated chars is magic, it used to show the user that the AI is typing just like human would type
+	AnimatedChars = "%c"
+	// this model is subject to changed in future
+	ModelAi = "gemini-pro"
 )
 
 // Defined constants for language
