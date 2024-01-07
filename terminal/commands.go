3decc01858df79d61bd7f078aa52288f018dfccf
H0llyW00dzZ <priv8@btz.pm>
2024-01-07 10:37:12 +0700

    [Commands] Update comment
    - [+] chore(commands.go): update comment to clarify the use of ":" prefix instead of "/" for commands
    - [+] docs(commands.go): add TODO comment for adding more commands in the future

--- terminal/commands.go
+++ terminal/commands.go
@@ -16,7 +16,7 @@ type CommandHandler func(session *Session) (bool, error)
 var commandHandlers = map[string]CommandHandler{
 	QuitCommand: handleQuitCommand,
 	//TODO: Will add more commands here, example: :help, :about, :credits, :k8s, syncing AI With Go Routines (Known as Gopher hahaha) etc.
-	//Note: In python, I don't think so it's possible hahaahaha
+	//Note: In python, I don't think so it's possible hahaahaha, also I am using prefix ":" instead of "/" is respect to git, fuck prefix "/" which is confusing for command line
 }
 
 // HandleCommand interprets the user input as a command and executes the associated action.
