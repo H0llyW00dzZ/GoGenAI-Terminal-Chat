c6e6aee7890d806c9eb06464a0a56e2ed52eab3a
H0llyW00dzZ <priv8@btz.pm>
2024-01-09 18:56:57 +0700

    Go Docs [Terminal] [Debug Or Error] Documentation RecoverFromPanic
    - [+] docs(terminal/debug_or_error.go): add documentation for RecoverFromPanic method

--- terminal/debug_or_error.go
+++ terminal/debug_or_error.go
@@ -56,3 +56,30 @@ func (l *DebugOrErrorLogger) Error(format string, v ...interface{}) {
 	colorizedMsg := ColorRed + msg + ColorReset // Apply red color to the entire message
 	l.logger.Println(colorizedMsg)
 }
+
+// RecoverFromPanic should be deferred at the beginning of a function or goroutine
+// to handle any panics that may occur. It logs the panic information with a
+// colorized output to distinguish the log message clearly in the terminal.
+//
+// The message "Recovered from panic:" is displayed in green, followed by the panic
+// value in red. This method ensures that the panic does not cause the program to crash
+// and provides a clear indication in the logs that a panic was caught and handled.
+//
+// Usage:
+//
+//	func someFunction() {
+//	    logger := terminal.NewDebugOrErrorLogger()
+//	    defer logger.RecoverFromPanic()
+//
+//	    // ... function logic that might panic ...
+//	}
+//
+// It is essential to call this method using defer right after obtaining a logger instance.
+// This ensures that it can catch and handle panics from anywhere within the scope of the
+// function or goroutine.
+func (l *DebugOrErrorLogger) RecoverFromPanic() {
+	if r := recover(); r != nil {
+		msg := fmt.Sprintf(RecoverGopher, ColorGreen, ColorReset, ColorRed, r, ColorReset)
+		l.logger.Println(msg)
+	}
+}
