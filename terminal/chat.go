3587a8d2a5efecfe17f14ef9b14f7fd4adffb011
H0llyW00dzZ <priv8@btz.pm>
2024-01-11 01:19:24 +0700

    Fix [Terminal] [Chats] Increase Chat History & Fix Prefix Context
    - [+] chore(chat.go): update MaxChatHistory constant to 15 from 5
    - [+] fix(chat.go): fix prefixesToRemove variable case from uppercase to lowercase

--- terminal/chat.go
+++ terminal/chat.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Note: This is subject to change (for example, it can be customized in commands). For now, it's stable. Additionally, a token is inexpensive since, with Google AI's Gemini-Pro model, the maximum is 32K tokens.
-const MaxChatHistory = 5 // Maximum number of messages to keep in history
+const MaxChatHistory = 15 // Maximum number of messages to keep in secret storage (Updated to 15 from 5 to be stored in the RAM's labyrinth, since it's still stable for terminal).
 
 // ChatHistory holds the chat messages exchanged during a session.
 // It provides methods to add new messages to the history and to retrieve
@@ -48,7 +48,7 @@ func (h *ChatHistory) AddMessage(user, text string) {
 func (h *ChatHistory) GetHistory() string {
 	// Note: if you're still wondering where this is all stored, it's in a place you won't find—somewhere in the RAM's labyrinth, hahaha!
 	// Define the prefixes to be removed
-	prefixesToRemove := []string{YouNerd, AiNerd}
+	prefixesToRemove := []string{youNerd, aiNerd}
 
 	for _, msg := range h.Messages {
 		sanitizedMsg := msg
