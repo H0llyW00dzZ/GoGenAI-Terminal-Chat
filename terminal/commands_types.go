fa1e66e3b6670f654cbe38a963186fc736502ddd
H0llyW00dzZ <priv8@btz.pm>
2024-01-11 13:56:23 +0700

    Chore [Terminal] [Command Types] update comments
    - [+] chore(commands_types.go): update comments for clarity and remove profanity

--- terminal/commands_types.go
+++ terminal/commands_types.go
@@ -3,9 +3,10 @@
 
 package terminal
 
-// Note: this list of commands already implemented,
-// now located here for easy maintain and avoiding a fucking complex that I don't fucking understand
-// like why everyone (mostly) write go so complex then always having problem
+// Note: This list of commands has already been implemented.
+// It is now located here for ease of maintenance and to avoid unnecessary complexity.
+// This approach questions why many developers write Go code in an overly complex manner (that I don't fucking understand),
+// which often leads to problems.
 type handleQuitCommand struct{}
 type handleHelpCommand struct{}
 type handleCheckVersionCommand struct{}
