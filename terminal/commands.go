917693b4e3147b17c24bcf45478190f465c8a0eb
H0llyW00dzZ <priv8@btz.pm>
2024-01-11 12:24:46 +0700

    Chore [Terminal] [Commands] Reorganizing Codebase (#58)
    - [+] chore(commands.go): Remove unused functions and comments
    - [+] feat(unimplemented.go): Add unimplemented functions for future use

--- terminal/commands.go
+++ terminal/commands.go
@@ -172,38 +172,6 @@ func handleHelpCommand(session *Session) (bool, error) {
 	return true, nil
 }
 
-// handleK8sCommand would be a handler function for a hypothetical ":k8s" command.
-// Note: This command is secret of H0llyW00dzZ (Original Author) would be used to interact with a Kubernetes cluster.
-func k8sCommand(session *Session) (bool, error) {
-	// currently unimplemented
-	return true, nil
-}
-
-// storageCommand would be a handler function for a hypothetical ":storage" command.
-func storageCommand(session *Session) (bool, error) {
-	// currently unimplemented
-	return true, nil
-}
-
-// savehistorytostorageCommand would be a handler function for a hypothetical ":save history" command.
-func savehistorytostorageCommand(session *Session) (bool, error) {
-	// currently unimplemented
-	return true, nil
-}
-
-// loadhistoryfromstorageCommand would be a handler function for a hypothetical ":load history" command.
-func loadhistoryfromstorageCommand(session *Session) (bool, error) {
-	// currently unimplemented
-	return true, nil
-}
-
-// reportshitFunctionthatTooComplexCommand would be a handler function for a hypothetical ":report" command.
-func reportshitFunctionthatTooComplex(session *Session) (bool, error) {
-	// currently unimplemented
-	// this would be used to report when goroutines panic in other side not because of this terminal LOL
-	return true, nil
-}
-
 // handleCheckVersionCommand checks if the current version of the software is the latest.
 // It updates the aiPrompt with either a confirmation that the current version is up to date
 // or with release notes for the latest version available.
