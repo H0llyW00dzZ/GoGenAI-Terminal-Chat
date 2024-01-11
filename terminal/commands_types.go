a1fa4a4b070bc278f954d694e239264eb0878e6f
H0llyW00dzZ <priv8@btz.pm>
2024-01-11 13:21:26 +0700

    Feat [Terminal] [Commands] command types
    - [+] feat(terminal): add command types for quit, help, and check version
    - [+] refactor(terminal): update command handlers to use struct pointers instead of struct literals

---
+++ terminal/commands_types.go
@@ -0,0 +1,8 @@
+package terminal
+
+// Note: this list of commands already implemented,
+// now located here for easy maintain and avoiding a fucking complex that I don't fucking understand
+// like why everyone (mostly) write go so complex then always having problem
+type handleQuitCommand struct{}
+type handleHelpCommand struct{}
+type handleCheckVersionCommand struct{}
