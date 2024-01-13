0a87799842605a94edecf0f762d8cea2b67f295b
H0llyW00dzZ <priv8@btz.pm>
2024-01-13 19:19:30 +0700

    Chore [Terminal] [GenAI] Pass Context Handling Triple Backticks
    - [+] chore(genai.go): add support for triple backticks in colorization
    - [+] fix(genai.go): remove double asterisks from the output

--- terminal/genai.go
+++ terminal/genai.go
@@ -130,11 +130,13 @@ func printResponse(resp *genai.GenerateContentResponse) string {
 				colorPairs := []string{
 					DoubleAsterisk, colors.ColorHex95b806,
 					SingleBacktick, colors.ColorYellow,
+					TripleBacktick, colors.ColorCyan24Bit,
 				}
 
 				keepDelimiters := map[string]bool{
 					DoubleAsterisk: false, // Remove double asterisks from the output
 					SingleBacktick: true,  // Keep single backticks in the output
+					TripleBacktick: true,  // Keep triple backticks in the output
 				}
 
 				// Colorize content that is surrounded by double asterisks or backticks
