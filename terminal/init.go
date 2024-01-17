fcb69ef07d3e6c96ea4175a5283875571a148bbf
H0llyW00dzZ <priv8@btz.pm>
2024-01-17 17:32:31 +0700

    Refactor [Terminal] commandHandlers to use CommandRegistry
    - [+] refactor(init.go): refactor commandHandlers to use CommandRegistry
    - [+] feat(init.go): initialize and register commands in CommandRegistry

--- terminal/init.go
+++ terminal/init.go
@@ -20,22 +20,7 @@ var apiKey string
 // logger is a package-level variable that can be used throughout the terminal package.
 var logger *DebugOrErrorLogger
 
-// commandHandlers maps command strings to their corresponding handler functions.
-// This allows for a scalable and maintainable way to manage chat commands.
-var commandHandlers = map[string]CommandHandler{
-	// Note: This map offers excellent scalability. For Example: You can easily add numerous commands without impacting
-	// the AI's performance or synchronization ai, such as `:quit` or `:checkversion`.
-	QuitCommand:        &handleQuitCommand{},
-	ShortQuitCommand:   &handleQuitCommand{}, // Short quit command
-	VersionCommand:     &handleCheckVersionCommand{},
-	HelpCommand:        &handleHelpCommand{},
-	ShortHelpCommand:   &handleHelpCommand{}, // Short help command
-	ClearCommand:       &handleClearCommand{},
-	SafetyCommand:      &handleSafetyCommand{},
-	AITranslateCommand: &handleAITranslateCommand{},
-	//TODO: Will add more commands here, example: :help, :about, :credits, :k8s, syncing AI With Go Routines (Known as Gopher hahaha) etc.
-	//Note: In python, I don't think so it's possible hahaahaha, also I am using prefix ":" instead of "/" is respect to git and command line, fuck prefix "/" which is confusing for command line
-}
+var registry *CommandRegistry
 
 // checkVersion is a package-level variable that holds the latest release information
 // fetched from the GitHub API. It is used to cache the details of the latest release
@@ -95,5 +80,20 @@ func init() {
 	logger = NewDebugOrErrorLogger()
 	// Compile the ANSI color code regular expression pattern.
 	ansiRegex = regexp.MustCompile(BinaryRegexAnsi)
+
+	// Initialize the command registry.
+	// Note: This NewCommandRegistry offers excellent scalability. For Example: You can easily add numerous commands without impacting
+	// the AI's performance or synchronization ai, such as `:quit` or `:checkversion`.
+	registry = NewCommandRegistry()
+	registry.Register(QuitCommand, &handleQuitCommand{})
+	registry.Register(ShortQuitCommand, &handleQuitCommand{})
+	registry.Register(VersionCommand, &handleCheckVersionCommand{})
+	registry.Register(HelpCommand, &handleHelpCommand{})
+	registry.Register(ShortHelpCommand, &handleHelpCommand{})
+	registry.Register(ClearCommand, &handleClearCommand{})
+	registry.Register(SafetyCommand, &handleSafetyCommand{})
+	registry.Register(AITranslateCommand, &handleAITranslateCommand{})
+	//TODO: Will add more commands here, example: :help, :about, :credits, :k8s, syncing AI With Go Routines (Known as Gopher hahaha) etc.
+	// Note: In python, I don't think so it's possible hahaahaha, also I am using prefix ":" instead of "/" is respect to git and command line, fuck prefix "/" which is confusing for command line
 
 }
