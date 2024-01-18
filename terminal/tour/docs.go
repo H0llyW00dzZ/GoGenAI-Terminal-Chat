09cd9646db1ef58ad489cabed99f4e0f48eae662
H0llyW00dzZ <priv8@btz.pm>
2024-01-18 16:00:30 +0700

    Update Documentation
    - [+] docs(terminal/tour): add reasons for creating the package and its purpose

--- terminal/tour/docs.go
+++ terminal/tour/docs.go
@@ -15,4 +15,10 @@
 // The functions in this package are intended to be examples of clean,
 // idiomatic Go code. They are suitable for educational purposes as well
 // as practical use in Go projects that require string manipulation.
+//
+// Reasons for creating this package:
+//
+//   - To provide solutions that handle strings in a way that is safe and compatible with
+//     operating system constraints, which may not manage constant strings effectively.
+//     Note: This is not a problem with Go, but rather a problem with the operating system so bad.
 package tour
