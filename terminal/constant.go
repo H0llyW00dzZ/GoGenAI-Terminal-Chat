bba44cff6ba239ba5ddf68cf2178292e3d5eb234
H0llyW00dzZ <priv8@btz.pm>
2024-01-22 16:11:39 +0700

    Chore [Constant] Refactor Error Message
    - [+] chore(constant.go): refactor error message variable names for clarity and consistency

--- terminal/constant.go
+++ terminal/constant.go
@@ -146,9 +146,9 @@ const (
 	ErrorInvalidApiKey                        = "Invalid API key: %v"
 	ErrorLowLevelNoResponse                   = "no response from AI service"
 	// List Error not because of this go codes, it literally google issue
-	Error500GoogleApi         = "googleapi: Error 500:"
-	ErrorGoogleInternal       = "Google Internal Error: %s"
-	ErrorFailedToSendMessages = "Failed to send message after retrying on internal server error: %v"
+	Error500GoogleApi                                           = "googleapi: Error 500:"
+	ErrorGoogleInternal                                         = "Google Internal Error: %s"
+	ErrorFailedToSendMessagesAfterRetryingonInternalServerError = "Failed to send message after retrying on internal server error: %v"
 	// List Error Figlet include high and low level error
 	ErrorStyleIsEmpty             = "style is empty"                   // low level
 	ErrorCharacterNotFoundinStyle = "character %q not found in style"  // low level
