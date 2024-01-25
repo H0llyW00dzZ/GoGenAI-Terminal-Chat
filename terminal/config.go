6a873050f098227f4dbe6d3167fd4512cdb31583
H0llyW00dzZ <priv8@btz.pm>
2024-01-26 06:22:01 +0700

    Implement "Option" Configuration for GenerativeModel (#200)
    * Implement "Option" Configuration for GenerativeModel
    
    * Fix Formatting "Import"

--- terminal/config.go
+++ terminal/config.go
@@ -3,6 +3,10 @@
 
 package terminal
 
+import (
+	genai "github.com/google/generative-ai-go/genai"
+)
+
 // ChatConfig encapsulates settings that affect the management of chat history
 // during a session with the generative AI. It determines the amount of chat history
 // retained in memory and the portion of that history used to provide context to the AI.
@@ -25,8 +29,7 @@ type ChatConfig struct {
 //
 // Returns:
 //
-//  *ChatConfig: A pointer to a ChatConfig instance populated with default settings.
-//
+//	*ChatConfig: A pointer to a ChatConfig instance populated with default settings.
 func DefaultChatConfig() *ChatConfig {
 	return &ChatConfig{
 		// Note: This history size is stable. It is automatically handled by the garbage collector.
@@ -36,5 +39,43 @@ func DefaultChatConfig() *ChatConfig {
 		HistorySize: 10, // Default to retaining the last 10 messages
 		// Note: HistorySendToAI currently is unimplemented, will implemented it later when I am free
 		HistorySendToAI: 10, // Default to sending the last 10 messages for AI context
+	}
+}
+
+// Option is a function type that takes a pointer to a GenerativeModel instance and applies a specific configuration to it.
+type Option func(m *genai.GenerativeModel)
+
+// WithTemperature sets the temperature parameter of the model.
+func WithTemperature(temperature float32) Option {
+	return func(m *genai.GenerativeModel) {
+		m.SetTemperature(float32(temperature))
+	}
+}
+
+// WithTopP sets the top_p parameter of the model.
+func WithTopP(topP float32) Option {
+	return func(m *genai.GenerativeModel) {
+		m.SetTopP(float32(topP))
+	}
+}
+
+// WithTopK sets the top_k parameter of the model.
+func WithTopK(topK int32) Option {
+	return func(m *genai.GenerativeModel) {
+		m.SetTopK(int32(topK))
+	}
+}
+
+// WithMaxOutputTokens sets the max_output_tokens parameter of the model.
+func WithMaxOutputTokens(maxOutputTokens int32) Option {
+	return func(m *genai.GenerativeModel) {
+		m.SetMaxOutputTokens(int32(maxOutputTokens))
+	}
+}
+
+// ApplyOptions applies the provided options to the given generative AI model.
+func ApplyOptions(m *genai.GenerativeModel, options ...Option) {
+	for _, option := range options {
+		option(m)
 	}
 }
