b8c5888c7ebfedbda0e80931c77036da389e3e37
H0llyW00dzZ <priv8@btz.pm>
2024-01-26 16:10:37 +0700

    Fix "import" and Go Docs Formatting (#201)

--- terminal/tour/binary.go
+++ terminal/tour/binary.go
@@ -3,14 +3,17 @@
 
 package tour
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // ConvertToBinary converts a string to its binary representation.
 // Each character of the input string is represented by its binary
 // equivalent, separated by spaces in the resulting string.
 //
 // For example:
-//   ConvertToBinary("Go") returns "1000111 1101111"
+//
+//	ConvertToBinary("Go") returns "1000111 1101111"
 func ConvertToBinary(input string) string {
 	var binaryRepresentation string
 	for _, runeValue := range input {
