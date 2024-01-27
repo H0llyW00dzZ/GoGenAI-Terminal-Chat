a90c361bf8cb6ab8f6b71c90685b6828f315fc83
H0llyW00dzZ <priv8@btz.pm>
2024-01-27 08:27:03 +0700

    Update Current Version
    - [+] chore(constant.go): update CurrentVersion constant to v0.5.8

--- terminal/constant.go
+++ terminal/constant.go
@@ -11,7 +11,7 @@ const (
 	GitHubAPIURL      = "https://api.github.com/repos/H0llyW00dzZ/GoGenAI-Terminal-Chat/releases/latest"
 	GitHubReleaseFUll = "https://api.github.com/repos/H0llyW00dzZ/GoGenAI-Terminal-Chat/releases/tags/%s"
 	// CurrentVersion represents the current version of the application.
-	CurrentVersion = "v0.5.7"
+	CurrentVersion = "v0.5.8"
 )
 
 // Defined constants for the terminal package
