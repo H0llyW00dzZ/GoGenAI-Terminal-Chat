e69c4a2595c1e33e197e6dcd9ec3b9bf60d336d4
H0llyW00dzZ <priv8@btz.pm>
2024-01-31 22:27:01 +0700

    Chore [Terminal] [Commands] Reorganize Codebase
    - [+] chore(commands.go): remove unused functions clearChatHistory, clearSummarizeHistory, and showChatStats
    - [+] feat(executecmd_stuff.go): move checkVersionAndGetPrompt function from commands.go to executecmd_stuff.go

--- terminal/commands.go
+++ terminal/commands.go
@@ -167,28 +167,6 @@ func (c *handleCheckVersionCommand) Execute(session *Session, parts []string) (b
 	return false, nil
 }
 
-// checkVersionAndGetPrompt checks if the current version of the software is the latest and informs the user accordingly.
-func (c *handleCheckVersionCommand) checkVersionAndGetPrompt() (aiPrompt string, err error) {
-	// Check if the current version is the latest.
-	isLatest, latestVersion, err := checkLatestVersionWithBackoff()
-	if err != nil {
-		return "", err
-	}
-
-	if isLatest {
-		aiPrompt = fmt.Sprintf(YouAreusingLatest, VersionCommand, ApplicationName, CurrentVersion)
-	} else {
-		// Fetch and format the release information for the latest version.
-		aiPrompt, err = fetchAndFormatReleaseInfo(latestVersion)
-		if err != nil {
-			return "", err
-		}
-	}
-
-	// Return the prompt to the caller.
-	return aiPrompt, nil
-}
-
 // Execute performs the ping operation on the provided IP address.
 // It uses system utilities to send ICMP packets to the IP and returns the result.
 //
@@ -242,32 +220,6 @@ func (cmd *handleClearCommand) HandleSubcommand(subcommand string, session *Sess
 		logger.Error(ErrorUnrecognizedSubcommandForClear, subcommand)
 		return false, nil
 	}
-}
-
-// clearChatHistory clears the chat history.
-func (cmd *handleClearCommand) clearChatHistory(session *Session) (bool, error) {
-	session.ChatHistory.Clear()
-	// Prepare the full message to be printed
-	clearMessage := ChatHistoryClear
-	showTokenCount := os.Getenv(SHOW_TOKEN_COUNT) == "true"
-	// Append token reset message if SHOW_TOKEN_COUNT is true
-	if showTokenCount {
-		totalTokenCount = 0 // Reset the total token count to zero
-		clearMessage += "\n" + ResetTotalTokenUsage
-	}
-	// Print the message(s) with timestamp and typing effect
-	logger.Any(clearMessage) // simplify
-	// Added back the context prompt after clearing the chat history
-	session.ChatHistory.AddMessage(AiNerd, ContextPrompt, session.ChatConfig)
-	return false, nil // Continue the session
-}
-
-// clearSummarizeHistory clears the summarized messages from the chat history.
-func (cmd *handleClearCommand) clearSummarizeHistory(session *Session) (bool, error) {
-	session.ChatHistory.ClearAllSystemMessages()
-	logger.Any(ChatSysSummaryMessages) // simplify
-	session.ChatHistory.AddMessage(AiNerd, ContextPrompt, session.ChatConfig)
-	return false, nil // Continue the session
 }
 
 // Execute processes the ":safety" command within a chat session.
@@ -452,23 +404,6 @@ func (cmd *handleStatsCommand) HandleSubcommand(subcommand string, session *Sess
 		logger.Error(ErrorUnrecognizedSubcommandForClear, subcommand)
 		return false, nil
 	}
-}
-
-// showChatStats displays the chat statistics in a session. It retrieves the statistics
-// from the session's ChatHistory and prints them to the console with a typing effect.
-// The typing delay is specified by the TypingDelay constant/variable.
-// The function prints a system message prefix with a timestamp before the stats.
-// After printing the stats, it continues the session without error.
-func (cmd *handleStatsCommand) showChatStats(session *Session) (bool, error) {
-	// Retrieve chat statistics from the session's ChatHistory.
-	stats := session.ChatHistory.GetMessageStats()
-
-	// Use the logger's Any method to print the statistics without colorization.
-	// The SYSTEMPREFIX is included directly in the formatted message.
-	logger.Any(ListChatStats,
-		stats.UserMessages, stats.AIMessages, stats.SystemMessages)
-
-	return false, nil // Continue the session without error.
 }
 
 func (cmd *handleTokeCountingCommand) Execute(session *Session, parts []string) (bool, error) {
