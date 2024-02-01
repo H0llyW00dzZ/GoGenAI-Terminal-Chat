daaad8a14244e144eac65203aba7612c124d9e97
H0llyW00dzZ <priv8@btz.pm>
2024-02-01 21:11:01 +0700

    Add Comment
    - [+] feat(config.go): add comment explaining advanced Go idiomatic pointer usage in WithSafetyOptions function

--- terminal/config.go
+++ terminal/config.go
@@ -130,6 +130,7 @@ func WithMaxOutputTokens(maxOutputTokens int32) (ModelConfig, error) {
 // Note: This is currently marked as TODO, as it's not used anywhere in the code. However, it would be advantageous to implement this feature in the future.
 // For instance, it could be used with Vertex AI.
 func WithSafetyOptions(safety *SafetySettings) ModelConfig {
+	// Note: This advanced idiomatic Go makes use of pointers hahaha.
 	return func(m *genai.GenerativeModel) {
 		safety.ApplyToModel(m)
 	}
