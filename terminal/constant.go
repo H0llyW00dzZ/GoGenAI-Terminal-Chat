6707dfd08bed5532fe5918f39b275892ba47c6ae
H0llyW00dzZ <priv8@btz.pm>
2024-02-16 04:35:59 +0700

    Bump Current Version
    - [+] chore(terminal/constant.go): bump CurrentVersion from v0.8.8 to v0.8.9

--- terminal/constant.go
+++ terminal/constant.go
@@ -12,7 +12,7 @@ const (
 	GitHubAPIURL      = "https://api.github.com/repos/H0llyW00dzZ/GoGenAI-Terminal-Chat/releases/latest"
 	GitHubReleaseFUll = "https://api.github.com/repos/H0llyW00dzZ/GoGenAI-Terminal-Chat/releases/tags/%s"
 	// CurrentVersion represents the current version of the application.
-	CurrentVersion = "v0.8.8"
+	CurrentVersion = "v0.8.9"
 )
 
 // Defined constants for the terminal package
