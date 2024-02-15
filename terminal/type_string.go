115f220be13ee7996320274a52cf523684424135
H0llyW00dzZ <priv8@btz.pm>
2024-02-16 06:46:59 +0700

    Implement Custom Error Type For ASCII Art (#375)
    * Feat [Terminal] [Type String] Custom Error Type
    
    - [+] feat(terminal/type_string.go): add new custom error type ErrorASCIIArt for ASCII art conversion errors
    
    * Feat [Explicit] [Terminal] [ASCII Art] "ErrorASCIIArt"
    
    - [+] feat(ascii_art.go): add ErrorASCIIArt and NewASCIIArtError functions
    - [+] refactor(ascii_art.go): replace fmt.Errorf with NewASCIIArtError
    - [+] refactor(ascii_art.go): change applyColor to return error along with string
    - [+] refactor(ascii_art.go): modify ToASCIIArt and buildOutput to handle new applyColor and NewASCIIArtError

---
+++ terminal/type_string.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 H0llyW00dzZ
+//
+// License: MIT License
+
+package terminal
+
+// ErrorASCIIArt is a custom error type for errors related to ASCII art conversion.
+type ErrorASCIIArt struct {
+	Message string
+}
